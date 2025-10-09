fb90890dbfc18bb688134e465060393f2fd4a982
PC_User <PC_User@192.168.10.8>
2025-10-09 14:55:07 +0900

    StaffOrderServiceに会員名関連の機能を追加

--- src/main/java/jp/ken/interiorshop/application/service/StaffOrderService.java
+++ src/main/java/jp/ken/interiorshop/application/service/StaffOrderService.java
@@ -6,20 +6,25 @@ import java.util.List;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import jp.ken.interiorshop.domain.entity.MemberEntity;
 import jp.ken.interiorshop.domain.entity.OrderEntity;
+import jp.ken.interiorshop.domain.repository.MemberRepository;
 import jp.ken.interiorshop.domain.repository.StaffOrderRepository;
+import jp.ken.interiorshop.presentation.form.MemberLoginForm;
 import jp.ken.interiorshop.presentation.form.OrderForm;
 
 @Service
 public class StaffOrderService {
 	
 	private StaffOrderRepository staffOrderRepository;
+	private MemberRepository memberRepository;
 	private ModelMapper FormMapper;
 	
 	public StaffOrderService(StaffOrderRepository staffOrderRepository,
+			MemberRepository memberRepository,
 			ModelMapper FormMapper) {
 		this.staffOrderRepository = staffOrderRepository;
-
+		this.memberRepository = memberRepository;
 		this.FormMapper = FormMapper;
 
 	}
@@ -39,12 +44,43 @@ public class StaffOrderService {
 		return formList;
 	}
 	
+	//注文履歴内の会員IDから会員名を取得
+	public List<MemberLoginForm> getMemberNameList (List<OrderForm> listOrderForm) throws Exception {
+		
+		List<MemberEntity> entityList = new ArrayList<>();
+		MemberEntity entity = null;
+		List<MemberLoginForm> formList = null;
+		
+		//引数のリスト内にある会員IDから会員情報を取得し、entityListに格納
+		for (OrderForm orderForm : listOrderForm) {
+			entity = memberRepository.findByMemberId(orderForm.getMemberId());
+			entityList.add(entity);
+		}
+		
+		//取得したデータをList<MemberLoginForm>型に変換
+		formList = convertName(entityList);
+		
+		return formList;
+	}
+	
 	private List<OrderForm> convert(List<OrderEntity> entityList) {
 		
 		List<OrderForm> formList = new ArrayList<OrderForm>();
 		
 		for(OrderEntity entity : entityList) {
 			OrderForm form = FormMapper.map(entity, OrderForm.class);
+			formList.add(form);
+		}
+		
+		return formList;
+	}
+	
+	private List<MemberLoginForm> convertName(List<MemberEntity> entityList) {
+		
+		List<MemberLoginForm> formList = new ArrayList<MemberLoginForm>();
+		
+		for(MemberEntity entity : entityList) {
+			MemberLoginForm form = FormMapper.map(entity, MemberLoginForm.class);
 			formList.add(form);
 		}
 		
