6c88c427c35aaf19a455ce6d487100e27c1f7322
PC_User <PC_User@192.168.10.8>
2025-10-09 12:02:37 +0900

    Merge remote-tracking branch 'origin/main' into fukuda

--- src/main/java/jp/ken/interiorshop/presentation/controller/StaffOrderController.java
+++ src/main/java/jp/ken/interiorshop/presentation/controller/StaffOrderController.java
@@ -3,10 +3,13 @@ package jp.ken.interiorshop.presentation.controller;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import jp.ken.interiorshop.application.service.StaffOrderService;
 import jp.ken.interiorshop.presentation.form.OrderForm;
+import jp.ken.interiorshop.presentation.form.StaffLoginForm;
 
 @Controller
 public class StaffOrderController {
@@ -18,11 +21,25 @@ public class StaffOrderController {
 	}
 
 	@GetMapping(value = "/stafforder")
-	public String OrderManagement() throws Exception{
+	public String OrderManagement(Model model) throws Exception{
 		
 		//DBから注文履歴を取得
 		List<OrderForm> listOrderForm = staffOrderService.getOrderList();
+		model.addAttribute("listOrderForm", listOrderForm);
 		
 		return "redirect:/staffOrderHistory";
+	}
+	
+	// 注文履歴確認画面表示
+	@GetMapping("/stafforder/history")
+	public String showOrderHistory(@SessionAttribute("loginStaff") StaffLoginForm staffLoginForm, Model model) 
+		throws Exception{
+		model.addAttribute("loginStaff", staffLoginForm);
+		
+//		//DBから注文履歴を取得
+		List<OrderForm> listOrderForm = staffOrderService.getOrderList();
+		model.addAttribute("listOrderForm", listOrderForm);
+		
+		return "staffOrderHistory";
 	}
 }
